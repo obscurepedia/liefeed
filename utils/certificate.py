bec0528f0a555004bea3c7890b22cd7a8a1e3143
Isaac Goldberg <yg288846@gmail.com>
2025-05-06 00:16:08 +0100

    update

--- utils/certificate.py
+++ utils/certificate.py
@@ -7,5 +7,5 @@ def generate_certificate(name, quiz_title, score):
     html = render_template("certificate.html", name=name, quiz_title=quiz_title, score=score, date=datetime.now().strftime("%B %d, %Y"))
     pdf_path = f"certificates/{name.replace(' ', '_')}_certificate.pdf"
     os.makedirs("certificates", exist_ok=True)
-    HTML(string=html).write_pdf(pdf_path)
+    HTML(string=html, base_url='.').write_pdf(pdf_path)
     return pdf_path
